ee48f98d9aa9dd6ce0dd08a559b292a90f991797
Yovany <yovany.lg@gmail.com>
2018-10-26 04:29:15 +0000

    First commit on soccer bot client

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const  nm = require('node-machine-id');
+const mqtt = require('mqtt');
+const MotorController = require('../../robotois-servo-controller');
+
+const motorController = new MotorController();
+
+let id = nm.machineIdSync({original: true});
+const clientId = `SoccerBot-${id}` || 'SoccerBot-01';
+const myTopic = `SoccerBots/${clientId}`;
+
+const client = mqtt.connect('mqtt://192.168.50.27', { clientId });
+
+//console.log(clientId);
+
+client.on('connect', function () {
+  client.subscribe(myTopic);
+});
+
+client.on('message', function(topic, message) {
+  //console.log(clientId);
+  const msgStr = message.toString();
+  const msgObj = JSON.parse(msgStr);
+  //console.log(msgObj);
+  motorController.drive(msgObj.x, msgObj.y, msgObj.r);
+});
